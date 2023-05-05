adf519ef0aee5fddb826d00475a1ba5b4d37d3dd
Andres Maneiro <amaneiro7@gmail.com>
2023-05-05 02:48:53 -0400

    logic of update

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -20,7 +20,16 @@ export const createEmployees = async (req, res) => {
   })
 }
 
-export const updateEmployees = (req, res) => res.send('eliminando empleados')
+export const updateEmployees = async (req, res) => {
+  const { id } = req.params
+  const { name, salary } = req.body
+  const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
+
+  if (result.affectedRows <= 0) return res.status(404).json({ message: 'Employee not found' })
+  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+
+  res.json(rows[0])
+}
 
 export const deleteEmployees = async (req, res) => {
   const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -6,7 +6,7 @@ const router = Router()
 router.get('/employees', getEmployees)
 router.get('/employees/:id', getEmployee)
 router.post('/employees', createEmployees)
-router.put('/employees/:id', updateEmployees)
+router.patch('/employees/:id', updateEmployees)
 router.delete('/employees/:id', deleteEmployees)
 
 export default router
